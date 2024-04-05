0839458660745c43e3a9dae03e3d2cabc973d10c
arafat-sabbir <arafatshabbir8@gmail.com>
2024-04-06 02:17:41 +0600

    Maked The SendOtp email A Service To Reuse Later

--- modules/user/controllers/userController.js
+++ modules/user/controllers/userController.js
@@ -1,75 +1,28 @@
 const asyncHandler = require("express-async-handler");
-const nodemailer = require("nodemailer");
+const bcrypt = require("bcrypt");
 const {
-  createOtp,
   findUserOtp,
   deleteOpt,
   getUserByData,
   updateUser,
+  sendOtpToEmail,
 } = require("../services/userService");
-const bcrypt = require("bcrypt");
-const Otp = require("../models/otpModel");
 
 const sendOtpEmail = asyncHandler(async (req, res) => {
   const { email } = req;
   try {
-    const transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: process.env.SMTP_PORT,
-      secure: false, // Use `true` for port 465, `false` for all other ports
-      auth: {
-        user: process.env.SENDER_EMAIL,
-        pass: process.env.SENDER_PASSWORD,
-      },
-    });
-
-    const otp = `${Math.floor(1000 + Math.random() * 9000)}`;
-    const messageOption = {
-      from: process.env.SENDER_EMAIL,
-      to: email,
-      subject: "Notification from Reviva",
-      html: `<section style="margin: 0; padding: 0; font-family: Arial, sans-serif; font-size: 16px; line-height: 1.4; color: #333; background-color: #f5f5f5;">
-      <table style="width: 100%; max-width: 600px; margin: 0 auto;">
-        <tr>
-          <td style="padding: 20px;">
-            <div style="text-align: center;">
-              <h3 style="font-weight: 600; color: #00b0e0; margin-top: 0;">REVIVA</h3>
-            </div>
-            <div style="margin-bottom: 10px;">
-              <p style="font-size: 16px; margin: 0;">Your Password Change Verification Otp is: <span style="font-weight: bold; font-size: 24px; color: #333; text-align:center ">${otp}</span> It Will Expire In <strong style="color:"#FF0000">1 Hour</strong/></p>
-            </div>
-            <div style="text-align: center; margin-top: 50px; font-size: 12px;">
-              <p>Powered by &copy; Reviva Ltd.</p>
-            </div>
-          </td>
-        </tr>
-      </table>
-    </section>`,
-    };
-
-    transporter.sendMail(messageOption, async (error, info) => {
-      if (!error) {
-        // Hash the OTP
-        const hashedOtp = await bcrypt.hash(otp, 10);
-        const otpData = {
-          email,
-          otp: hashedOtp,
-          createdAt: Date.now(),
-          expiredAt: Date.now() + 3600000, // Adjust expiry time here
-        };
-        const newOtp = await createOtp(otpData);
-        res.status(200).json({
-          message: "Otp Sent Successfully",
-          success: true,
-          data: newOtp,
-        });
-      } else {
-        console.log(error, "from nodemailer");
-      }
+    const otp = await sendOtpToEmail(email);
+    res.status(200).json({
+      message: "Otp Send Successfully to Email",
+      success: true,
+      data: otp,
     });
   } catch (error) {
-    console.error("Error occurred while sending email:", error);
-    res.status(500).json({ error: "Internal Server Error" });
+    res.status(500).json({
+      message: "Error While Sending Otp To Email",
+      success: false,
+      error,
+    });
   }
 });
 
@@ -94,16 +47,18 @@ const verifyOtpAndUpdate = asyncHandler(async (req, res) => {
   if (optMatched) {
     const otpExpiry = userOtp?.expiredAt > Date.now();
     if (!otpExpiry) {
-      res.status(401).json({ message: "Otp Has Been Expired", success: false });
-      return await deleteOpt({ email });
+      await deleteOpt({ email });
+      return res
+        .status(401)
+        .json({ message: "Otp Has Been Expired", success: false });
     } else {
       const hashedNewPassword = await bcrypt.hash(newPassword, 10);
       await updateUser({ email }, { password: hashedNewPassword });
-      res.status(200).json({
+      await deleteOpt({ email });
+      return res.status(200).json({
         message: "Otp Verified And User Updated SuccessFully",
         success: true,
       });
-      return await deleteOpt({ email });
     }
   }
 });
--- modules/user/services/userService.js
+++ modules/user/services/userService.js
@@ -1,4 +1,6 @@
 const Otp = require("../models/otpModel");
+const nodemailer = require("nodemailer");
+const bcrypt = require("bcrypt");
 const User = require("../models/userModel");
 
 const createUser = async (userData) => {
@@ -54,13 +56,71 @@ const getUserByData = async (data) => {
 
 const updateUser = async (email, userData) => {
   try {
-    const user = await User.findOneAndUpdate( email, userData);
+    const user = await User.findOneAndUpdate(email, userData);
     if (!user) {
       throw new Error("Error While Updating User");
     }
     return user;
   } catch (error) {
     throw new Error("Error While Updating User: " + error.message);
+  }
+};
+
+const sendOtpToEmail = async (email) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      secure: false, // Use `true` for port 465, `false` for all other ports
+      auth: {
+        user: process.env.SENDER_EMAIL,
+        pass: process.env.SENDER_PASSWORD,
+      },
+    });
+
+    const otp = `${Math.floor(1000 + Math.random() * 9000)}`;
+    const messageOption = {
+      from: process.env.SENDER_EMAIL,
+      to: email,
+      subject: "Notification from Reviva",
+      html: `<section style="margin: 0; padding: 0; font-family: Arial, sans-serif; font-size: 16px; line-height: 1.4; color: #333; background-color: #f5f5f5;">
+      <table style="width: 100%; max-width: 600px; margin: 0 auto;">
+        <tr>
+          <td style="padding: 20px;">
+            <div style="text-align: center;">
+              <h3 style="font-weight: 600; color: #00b0e0; margin-top: 0;">REVIVA</h3>
+            </div>
+            <div style="margin-bottom: 10px;">
+              <p style="font-size: 16px; margin: 0;">Your Password Change Verification Otp is: <span style="font-weight: bold; font-size: 24px; color: #333; text-align:center ">${otp}</span> It Will Expire In <strong style="color:"#FF0000">1 Hour</strong/></p>
+            </div>
+            <div style="text-align: center; margin-top: 50px; font-size: 12px;">
+              <p>Powered by &copy; Reviva Ltd.</p>
+            </div>
+          </td>
+        </tr>
+      </table>
+    </section>`,
+    };
+
+    transporter.sendMail(messageOption, async (error, info) => {
+      if (!error) {
+        // Hash the OTP
+        const hashedOtp = await bcrypt.hash(otp, 10);
+        const otpData = {
+          email,
+          otp: hashedOtp,
+          createdAt: Date.now(),
+          expiredAt: Date.now() + 3600000, // Adjust expiry time here
+        };
+        const newOtp = await createOtp(otpData);
+        return newOtp;
+      } else {
+        throw new Error("Error Sending Otp Email: " + error.message);
+      }
+    });
+  } catch (error) {
+    console.error("Error occurred while sending email:", error);
+    throw new Error("Error occurred while sending email: " + error.message);
   }
 };
 
@@ -71,4 +131,5 @@ module.exports = {
   findUserOtp,
   deleteOpt,
   updateUser,
+  sendOtpToEmail
 };
